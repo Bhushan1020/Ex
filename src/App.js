ef1705d450c5122783cf0f3e1d00953ab0627a6d
Bhushan1020 <bhushannarawade2003@gmail.com>
2024-03-16 08:24:55 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './component/Home';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Main from './component/Main';
+import {Route, Routes} from 'react-router-dom';
+import Events from './component/Events';
+import Footer from './component/Footer';
+import Contact from './component/Contact';
+import AboutUs from './component/AboutUs';
+import FAQ from './component/FAQ';
+import Team from './component/Team';
+import Admin from './component/Admin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='my'>
+    <Home/>
+    <Routes>
+      <Route path='/' element={<Main/>}></Route>
+      <Route path='/events' element={<Events/>}></Route>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/aboutus' element={<AboutUs/>}/>
+      <Route path='/faq' element={<FAQ/>}/>
+      <Route path='/team' element={<Team/>}/>
+      <Route path='/admin' element={<Admin/>}/>
+    </Routes>
+    <Footer/>
     </div>
+    </>
   );
 }
 
