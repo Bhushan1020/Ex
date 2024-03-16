63986ad25947afc2da5f4b9efb4bef44eab7d836
Bhushan1020 <bhushannarawade2003@gmail.com>
2024-03-16 09:19:15 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import Home from './component/Home';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import Main from './component/Main';
-import {BrowserRouter as Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Events from './component/Events';
 import Footer from './component/Footer';
 import Contact from './component/Contact';
 import AboutUs from './component/AboutUs';
 import FAQ from './component/FAQ';
 import Team from './component/Team';
-import Admin from './component/Admin';
 
 function App() {
   return (
@@ -23,7 +22,6 @@ function App() {
       <Route path='/aboutus' element={<AboutUs/>}/>
       <Route path='/faq' element={<FAQ/>}/>
       <Route path='/team' element={<Team/>}/>
-      <Route path='/admin' element={<Admin/>}/>
     </Routes>
     <Footer/>
     </div>
